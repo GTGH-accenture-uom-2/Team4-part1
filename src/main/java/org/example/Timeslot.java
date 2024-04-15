b5f080d56fcbcdfd871e44854aeb50ce5b342327
Matina <matinampoz@gmail.com>
2024-04-15 09:09:01 +0300

    add clas timeslot

---
+++ src/main/java/org/example/Timeslot.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Timeslot {
+    private int day;
+    private int month;
+    private int year;
+    private int hour;
+    private int minutes;
+    private int startMinute;
+    private int endMinute;
+    private Doctor doc;
+}
