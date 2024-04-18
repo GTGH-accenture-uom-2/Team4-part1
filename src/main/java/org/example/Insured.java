3e56b6dcc653e7d10a993eed48fd4986e21cb68a
Matina <matinampoz@gmail.com>
2024-04-18 08:24:51 +0300

    i made somes changes, but need i help

--- src/main/java/org/example/Insured.java
+++ src/main/java/org/example/Insured.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Map;
+
 public class Insured extends Person{
     private String email;
     private String afm;
     private String birthdate;
+    private VaccinationCenter vacCenter;
 
     public Insured () { }
     public Insured(String name, String surname, String amka, String email, String afm, String birthdate) {
@@ -34,8 +37,37 @@ public class Insured extends Person{
     public void setBirthdate(String birthdate) {
         this.birthdate = birthdate;
     }
+
+    public VaccinationCenter selectVacCenter{
+        //ebala kai pedio VacCenter sta attributes
+        //code goes here
+    }
+
     public void print() {
         System.out.println("name: "+ getName() + ", "+ "surname:" + getSurname() + ", " + "amka:" + getAmka() + ","+
                 "email:" + email + ", " + "afm:" + afm + "," + "birthdate:" + birthdate);
+    }
+
+    public Timeslot selectTimeslot(VaccinationCenter vacCenter){
+        //na ginei mia getTimeslot stin vaccenter??
+        //την προσεγγισα με τετοιον τροπο ωστε να ειναι HasMap τα timeslots με boolen
+        // που δειχνει αν ειναι κατηλλειμενο το timeslot
+
+        System.out.println("Παρακαλώ επιλέξτε έναν αριθμό από τον παρακάτω κατάλογο:");
+
+        int index = 1;
+        for (Map.Entry<Timeslot, Boolean> entry : vacCenter.getTimeslot().entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+            index++;
+        }
+
+        int choice = scanner.nextInt();
+
+        if (choice < 1 || choice > vacCenter.getTimeslot().size()) {
+            System.out.println("Μη έγκυρη επιλογή.");
+            return null;
+        }
+
+        //εκτυπωση πληροφοριων του timeslot που επελεξε, πλιζ χελπ!
     }
 }
